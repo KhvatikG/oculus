e176e9c8efd24f6e057b5fb6b048afd55ab73445
Garik <khvatik.igor@gmail.com>
2024-12-11 16:10:58 +0300

    fix: path to file .env and name of username variable

--- src/config.py
+++ src/config.py
@@ -4,10 +4,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     BOT_TOKEN: str
     ADMIN_IDS: list[int] = []
-    USERNAME: str
+    USERNAME_: str
 
     model_config = SettingsConfigDict(
-        env_file='../.env',
+        env_file='./.env',
         env_file_encoding='utf-8'
     )
 
